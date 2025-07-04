22bfb3cd0d1b9ad7952387670a9781a7589ea173
jreniel <jreniel@gmail.com>
2025-07-04 14:12:20 -0400

    git subrepo clone git@github.com:jreniel/schismrs-bctides.git src/bctides
    subrepo:
      subdir:   "src/bctides"
      merged:   "c7c7b61"
    upstream:
      origin:   "git@github.com:jreniel/schismrs-bctides.git"
      branch:   "main"
      commit:   "c7c7b61"
    git-subrepo:
      version:  "0.4.9"
      origin:   "https://github.com/ingydotnet/git-subrepo"
      commit:   "4f60dd7"

---
+++ src/bctides/src/lib.rs
@@ -0,0 +1,10 @@
+pub mod bctides;
+pub mod bctypes;
+pub mod tidefac;
+pub mod tides;
+
+pub use bctypes::ElevationConfig;
+pub use bctypes::SalinityConfig;
+pub use bctypes::TemperatureConfig;
+pub use bctypes::VelocityConfig;
+pub use tidefac::tidefac;
